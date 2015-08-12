6d683eb6fa65cfe0435e81ab0338c470dce8829f
Jenn Leung <j.leung91@gmail.com>
2015-08-12 16:42:28 -0700

    added navigator

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 $('#submitBtn').on('click', function (){
 	submitForm();
+  
 });
 
 $( "#mainForm" ).submit(function( event ) {
@@ -24,23 +25,54 @@ $( "#mainForm" ).submit(function( event ) {
 	if (!("autofocus" in document.createElement("input"))) {
 	    $("#textBox").focus();
 	}
-	function submitForm(){
+  $(function() {
+      navigator.geolocation.getCurrentPosition(function(data) {
+          latitude = data["coords"]["latitude"];
+          longitude = data["coords"]["longitude"];
+          console.log(latitude+","+longitude);
+      }, function(error) {console.error(error)});
 
+    });
+	function submitForm(){
+    
+    
 		var request = {
-			query: $('#textBox').val()
+      location: {latitude,longitude}, 
+			query: $('#textBox').val(),
+      types: ['store']
 		}
 		service.textSearch(request, callback);
-
+    
 	}
 	//Enter Button function
 	$("input").keypress(function(event) {
 	    if (event.which == 13) {
 	        
-	        //*****ASK GABE
+	        
 	        $('#mainForm').submit(submitForm());
           event.preventDefault();
 	    }
 	});
+  
+  if (navigator.geolocation) {
+  console.log('Geolocation is supported!');
+    }
+    else {
+      console.log('Geolocation is not supported for this Browser/OS version yet.');
+    }
+
+
+
+// window.onload = function() {
+//   var startPos;
+//   var geoSuccess = function(position) {
+//     startPos = position;
+//     debugger
+//     $('#startLat').innerHTML = startPos.coords.latitude;
+//     $('#startLon').innerHTML = startPos.coords.longitude;
+//   };
+//   navigator.geolocation.getCurrentPosition(geoSuccess);
+// };
 	// if i want to include a map
 	// function callback(results, status) {
 	//   if (status == google.maps.places.PlacesServiceStatus.OK) {
