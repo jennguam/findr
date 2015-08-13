881d5ea9606fcb3455caaf511a44e7342a32287c
Jenn Leung <j.leung91@gmail.com>
2015-08-13 16:27:16 -0700

    got yes and no buttons styled and working to grab location

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,68 +1,82 @@
-$('#submitBtn').on('click', function (){
-	submitForm();
-  
+$(function() {
+  setupHandlers();
+  $("#eatInput").focus();
+
+  navigator.geolocation.getCurrentPosition(
+    function(data) {
+      window.latitude = data.coords.latitude;
+      window.longitude = data.coords.longitude;
+
+      $.ajax({
+          url: "https://maps.googleapis.com/maps/api/geocode/json",
+          type: "GET",
+          data: {
+            latlng: window.latitude + "," + window.longitude, 
+            sensor: true, 
+            key:"AIzaSyDZymPd6_9qWy_mhMnZvmU_-SEw5cj2jds"
+          }
+      }).then(
+        function(data){
+          window.locationValue = data.results[1].formatted_address
+          $('.city_name').text(data.results[1].address_components[0].long_name);
+        }
+      );
+    },
+    function(error) {
+      console.error(error.toString());
+    }
+  );
 });
 
-$( "#mainForm" ).submit(function( event ) {
-  event.preventDefault();
-});
-//Autofocus to the textbox
-if (!("autofocus" in document.createElement("input"))) {
-    $("#textBox").focus();
-}			
-	var map = new google.maps.Map(document.createElement('div'))
-	var service = new google.maps.places.PlacesService(map);
-  
-	var callback = function(data) {
-    $('.resultName').html("");
-    $('.resultRating').html("");
-		for (var i = 0; i < 10; i++) {
 
-		    $('.resultName').append('<span id="name">'+data[i].name+'</span><br>');
-		    $('.resultRating').append('<span id="rating">'+data[i].rating+'</span><br>');
-		   
-		}
-		data.length=0;
-		console.log(data);
-	}
-  var pyrmont = new google.maps.LatLng(37.7629571, -122.46647790000002);
-
-  $(function() {
-      navigator.geolocation.getCurrentPosition(function(data) {
-          window.latitude = data.coords.latitude;
-          window.longitude = data.coords.longitude;
-          console.log(window.latitude);
-          console.log(window.longitude);
-          //console.log( window['coordinates']['latitude']+","+window['coordinates']['longitude']);
-
-      }, function(error) {console.error(error);$('#question').text("What would you like to eat?","Oops! We can't quite get your location.")});
-
+var setupHandlers = function() {
+  $("#eatForm").submit(function(e) {
+    e.preventDefault();
+    window.eatValue = $("#eatInput").val();
+    $("#eatForm").hide();
+    $("#question").hide();
+    $("#locationQ").show();
   });
-  
-	function submitForm(){
+  $("#noBtn").on("click", function(e) {
+    $("#locationForm").show();
+    $("#locationInput").focus();
+  });
+  $("#locationForm").submit(function(e) {
+    e.preventDefault();
+    window.locationValue = $("#locationInput").val();
+    $("#locationForm").html("<h2>RESULTS</h2>");
+    doSearch();
+  });
+  $("#yesBtn").on("click", function(e) {
+    doSearch();
+  });
+}
 
-		var request = {
-      location: pyrmont,
-			query: $('#textBox').val(),
-      radius: '500',
-      types: ['restaurant'],
-		}
-		service.textSearch(request, callback);
-    
-	}
-	//Enter Button function
-	$("input").keypress(function(event) {
-	    if (event.which == 13) {
-	        $('#mainForm').submit(submitForm());
-          event.preventDefault();
-	    }
-	});
-  
-  if (navigator.geolocation) {
-    console.log('Geolocation is supported!');
-  }
-  else {
-      console.log('Geolocation is not supported for this Browser/OS version yet.');
-  }
+var doSearch = function() {
+  if (window.latitude !== null && typeof window.latitude !== 'undefined')
+    var coordinates = new google.maps.LatLng(window.latitude, window.longitude);
+  else 
+    console.error("we cant get user location.");
+	var request = {
+    location: coordinates,
+		query: window.eatValue + " " + window.locationValue,
+    radius: '500',
+    types: ['restaurant']
+	};
+  var callback = function(data) {
+    if(data.length == 0)
+      console.error("we have no results from apiz")
+    else {
+      $('.resultName').html("");
+      $('.resultRating').html("");
+      for (var i = 0; i < 10; i++) {
+        $('.resultName').append('<span id="name">'+data[i].name+'</span><br>');
+        $('.resultRating').append('<span id="rating">'+data[i].rating+'</span><br>');
+      }
+    }
+  };
 
-
+  var map = new google.maps.Map(document.createElement('div'))
+  var service = new google.maps.places.PlacesService(map);
+	service.textSearch(request, callback);
+}
