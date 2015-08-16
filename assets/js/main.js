bb7c9779931382b26da74e2e8b667a27b58a989d
Gabe G'Sell <gabe@hipmunk.com>
2015-08-16 02:26:17 -0700

    markers drop one by one, no button works, ---- for undefined, loading spinner for city name

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,8 +18,10 @@ $(function() {
       }).then(
         function(data){
           window.locationValue = data.results[0].formatted_address;
-          // console.log(window.locationValue);
-          // console.log(data.results)
+          if($('#loadingSpinner').is(':visible')) {
+            $('#loadingSpinner').hide();
+            $('#question2').show();
+          }
           $('.city_name').text(data.results[1].address_components[0].long_name);
         }
       );
@@ -38,24 +40,28 @@ var setupHandlers = function() {
     $("#eatForm").hide();
     $("#question").hide();
     $("#locationQ").show();
+    if($(".city_name").is(':empty'))
+      $("#loadingSpinner").show();
+    else
+      $("#question2").show();
   });
   $("#noBtn").on("click", function(e) {
+    $("#locationQ").hide();
+    $("#locationQ2").show();
     $("#locationForm").show();
     $("#locationInput").focus();
     $('#yesBtn').hide();
     $('#noBtn').hide();
-
   });
   $("#locationForm").submit(function(e) {
     e.preventDefault();
     window.locationValue = $("#locationInput").val();
-    $("#locationForm").html("<h2>RESULTS</h2>");
     doSearch();
+    $("#locationQ2").hide();
+    $("#locationForm").hide();
   });
   $("#yesBtn").on("click", function(e) {
     doSearch();
-    $("#map").show();
-    google.maps.event.trigger(map, 'resize');
     $("#yesBtn").hide();
     $("#noBtn").hide();
     $("#question2").hide();
@@ -65,12 +71,12 @@ var setupHandlers = function() {
 var doSearch = function() {
   if (window.latitude !== null && typeof window.latitude !== 'undefined')
     var coordinates = new google.maps.LatLng(window.latitude, window.longitude);
-  else 
+  else {
     $("#question2").html("Sorry! We can't seem to find your location.");
     $("#locationInput").focus();
     $("#map").hide();
+  }
 
-   console.error("we cant get user location.");
 	var request = {
     location: coordinates,
 		query: window.eatValue + " " + window.locationValue,
@@ -78,42 +84,63 @@ var doSearch = function() {
     types: ['restaurant']
 	};
   var callback = function(data, status) {
-    if(data.length == 0)
+    if (status !== google.maps.places.PlacesServiceStatus.OK) {
+      console.error("google api failed with status " + status);
+      return;
+    }
+    if(data.length == 0) {
       console.error("we have no results from API");
+      return;
+    }
     else {
       $('.resultName').html("");
       $('.resultRating').html("");
       for (var i = 0; i < 10; i++) {
         $('.resultName').append('<span id="name">'+data[i].name+'</span><br>');
-        $('.resultRating').append('<span id="rating">'+data[i].rating+'</span><br>');
+        if(typeof data[i].rating !== "undefined" && data[i].rating !== null)
+          rating = data[i].rating
+        else
+          rating = "----"
+        $('.resultRating').append('<span id="rating">'+rating+'</span><br>');
       }
-      if (status === google.maps.places.PlacesServiceStatus.OK) {
-        var infowindow = new google.maps.InfoWindow();
-        var bounds = new google.maps.LatLngBounds();
-        for (var i = 0; i < 10; i++) {
-          bounds.extend(createMarker(data[i], infowindow));
-        }
-        map.fitBounds(bounds);
+      var infowindow = new google.maps.InfoWindow();
+      bounds = new google.maps.LatLngBounds();
+      for (var i = 0; i < 10; i++) {
+        bounds.extend(createMarker(data[i], infowindow));
       }
+      $("#map").show();
+      google.maps.event.trigger(map, 'resize');
+      map.fitBounds(bounds);
+      $('html,body').scrollTop(0);
     }
   };
 
   var service = new google.maps.places.PlacesService(map);
 	service.textSearch(request, callback);
-  console.log("COORDINATES"+coordinates);
 }
 
 var map;
+var bounds
 
 function initMap() {
+    if (window.latitude !== null && typeof window.latitude !== 'undefined') {
+      lat = window.latitude;
+      lng = window.longitude;
+    }
+    else {
+      lat = 37.7822;
+      lng = -122.4167;
+    }
+
     map = new google.maps.Map($('#map')[0], {
-      center: {lat: 37.7833, lng: -122.4167},
+      center: {lat: lat, lng: lng},
       zoom: 15
     });
 }
 
+var count = 0;
+
 function createMarker(place, infowindow) {
-  //var myLatLng = {lat: place.geometry.location.G, lng: place.geometry.location.K};
   var positionLoc = new google.maps.LatLng(place.geometry.location.G,place.geometry.location.K);
   
   var marker = new google.maps.Marker({
@@ -122,7 +149,9 @@ function createMarker(place, infowindow) {
     title: place.name
   });
 
-  marker.setMap(map);
+  setTimeout(function() {
+    marker.setMap(map);
+  }, count += 100);
 
   google.maps.event.addListener(marker, 'click', function() {
     infowindow.setContent(place.name);
